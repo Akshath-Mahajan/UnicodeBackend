bf138335aa931b4df09592a6493fa3a33100fdd0
Akshath <akshathmahajan13@gmail.com>
2020-09-13 17:58:42 +0530

    Customised admin page for Tasks
    Each task is now shown by it's title, deadline and completion status
    in the admin page.
    Two new features added to the Tasks page -
        1) Mark complete - To mark tasks as complete from admin page
        2) Mark incomplete - To mark tasks as incomplete from admin page

--- ProgressionTimeline/TaskManager/admin.py
+++ ProgressionTimeline/TaskManager/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Task, List
+from django.contrib import messages 
+class TaskAdmin(admin.ModelAdmin): 
+    list_display = ('title', 'deadline', 'complete')
+    def complete(self, request, queryset): 
+        queryset.update(complete = 1) 
+        messages.success(request, "Selected Record(s) marked as complete.")
+    def incomplete(self, request, queryset): 
+        queryset.update(complete = 0) 
+        messages.success(request, "Selected Record(s) marked as incomplete.") 
+    admin.site.add_action(complete, "Mark Complete")
+    admin.site.add_action(incomplete, "Mark Incomplete") 
 
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
 admin.site.register(List)
 # Register your models here.
 
